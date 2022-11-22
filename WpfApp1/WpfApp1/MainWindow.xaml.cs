407db6c7d5d7d6a5ed02761a449fb6920cc339ca
RimasBerz <116100732+RimasBerz@users.noreply.github.com>
2022-11-22 20:08:45 +0200

    Add files via upload

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static int check;
+        public int Check { get; set; }
+        public void CheckRemove()
+        {
+            check++;
+            b1.Click += Button_Click;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            b1.Click += Button_Click;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            b1.Content = Convert.ToString(check);
+
+            CheckRemove();
+        }
+    }
+}
